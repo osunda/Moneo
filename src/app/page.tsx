f1e1af31e02b2b5e7439ebe7b982e824734e8e97
Gunnar Bjornson <gunnarv1@proton.me>
2025-03-01 22:54:44 -0400

    holy dhit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Features from "../components/Features";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
 import SupportedChains from '@/components/SupportedChains';
-import Footer from "../components/Footer";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
